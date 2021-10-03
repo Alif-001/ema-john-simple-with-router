0c67542a4048374b5784f67b63a83f6ebba8466b
MD. OMOR FARUK ALIF <alifh8282@gmali.com>
2021-10-03 23:37:54 +0600

    done

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import OrderReview from "./components/OrderReview/OrderReview";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Inventory from "./components/Inventory/Inventory";
 import NotFound from "./components/NotFound/NotFound";
+import PlaceOrder from "./components/PlaceOrder/PlaceOrder";
 
 function App() {
   return (
@@ -24,6 +25,9 @@ function App() {
           </Route>
           <Route path="/inventory">
             <Inventory></Inventory>
+          </Route>
+          <Route path="/placeorder">
+            <PlaceOrder></PlaceOrder>
           </Route>
           <Route path="*">
             <NotFound></NotFound>
